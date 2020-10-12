3fbebcfeafb2b088c3feed1ae872c637dc3dfe2f
Alan dos Anjos Farias <farias_alan@outlook.com>
2020-10-12 11:27:35 -0300

    teste inicial OK

--- src/main/java/com/festacorp/gestaofesta/GestaoFestaApplication.java
+++ src/main/java/com/festacorp/gestaofesta/GestaoFestaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GestaoFestaApplication {
 
-	public static void main(String[] args) {
+	public static  void main(String[] args) {
 		SpringApplication.run(GestaoFestaApplication.class, args);
 		
 	}
