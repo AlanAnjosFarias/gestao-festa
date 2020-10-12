ac49efb6bb136732a0c5998cbd3b17921c1e8461
Alan dos Anjos Farias <farias_alan@outlook.com>
2020-10-12 13:49:25 -0300

    BD

--- src/main/java/com/festacorp/gestaofesta/controller/ConvidadosController.java
+++ src/main/java/com/festacorp/gestaofesta/controller/ConvidadosController.java
@@ -1,13 +1,33 @@
 package com.festacorp.gestaofesta.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.festacorp.gestaofesta.model.Convidado;
+import com.festacorp.gestaofesta.repository.Convidados;
 
 @Controller
+//@RequestMapping("/convidados") //com essa anotation podemos remover ("/convidados") dos metodos da classe
 public class ConvidadosController {
 
+	@Autowired
+	private Convidados convidados;
+
 	@GetMapping("/convidados")
-	public String listar() {
-		return "ListaConvidados";
+	public ModelAndView listar() {
+		ModelAndView modelAndView = new ModelAndView("ListaConvidados"); // "ListaConvidados"nome da view/arquivo html
+		modelAndView.addObject("convidados", convidados.findAll()); // "convidados" metodo GET do browser
+		modelAndView.addObject(new Convidado());
+		return modelAndView;
+	}
+
+	@PostMapping("/convidados")
+	public String salvar(Convidado convidado) {
+		this.convidados.save(convidado);
+		return "redirect:/convidados";
+
 	}
 }
---
+++ src/main/java/com/festacorp/gestaofesta/repository/Convidados.java
@@ -0,0 +1,9 @@
+package com.festacorp.gestaofesta.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.festacorp.gestaofesta.model.Convidado;
+
+public interface Convidados extends JpaRepository<Convidado, Long>{
+
+}
